c0c208e66c5ad830caed5770bd7b2d5930ca4ec9
JGB007 <17600396592@163.com>
2017-08-10 15:04:37 +0800

    ceshi

---
+++ app/src/main/java/com/example/administrator/myactivity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.administrator.myactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn= (Button) findViewById(R.id.button);
+    }
+
+
+}
