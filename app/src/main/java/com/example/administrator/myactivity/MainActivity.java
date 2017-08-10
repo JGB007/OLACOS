ae12efc239cdf0f15f528fa44966e3138e580977
JGB007 <17600396592@163.com>
2017-08-10 15:10:09 +0800

    ceshi

--- app/src/main/java/com/example/administrator/myactivity/MainActivity.java
+++ app/src/main/java/com/example/administrator/myactivity/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn;
+    private Button btn;//
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
